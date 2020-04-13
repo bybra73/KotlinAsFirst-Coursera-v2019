4e6d8857cae1a2b99677a586fd634dcb205520e2
Bybra73 <bybra73@gmail.com>
2020-04-14 02:47:43 +0300

    сделал все простые задачи урока 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,7 +2,11 @@
 
 package lesson3.task1
 
+import lesson1.task1.sqr
+import java.lang.Integer.max
+import java.lang.Integer.min
 import kotlin.math.sqrt
+
 
 /**
  * Пример
@@ -67,7 +71,15 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var count = 0
+    var new_n: Int = n
+    do {
+        new_n /= 10
+        count++
+    } while (new_n != 0)
+    return count
+}
 
 /**
  * Простая
@@ -75,7 +87,23 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int
+// if (n <= 2) 1
+// else fib(n - 2) + fib(n - 1)
+{
+    var result1 = 1
+    var result2 = 1
+    var result = 1
+    if (n <= 2)
+    else {
+        for (i in 3..n) {
+            result = result1 + result2
+            result1 = result2
+            result2 = result
+        }
+    }
+    return result
+}
 
 /**
  * Простая
@@ -83,21 +111,39 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    var count = 0
+    do count++
+    while (((max(m, n) * count) % min(m, n)) != 0)
+    return (max(m, n) * count)
+}
+
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var MinDiv = 2
+    if (n % 2 == 0)
+    else for (m in 3..sqrt(n.toDouble()).toInt() step 2) if (n % m == 0) {
+        MinDiv = m
+        break
+    } else MinDiv = n
+    return MinDiv
+}
+
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var newn = n
+    return (newn / minDivisor(newn))
+}
 
 /**
  * Простая
@@ -106,7 +152,18 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var min_num: Int = min(m, n)
+    var temp1: Int
+    var difference: Int = max(m, n) - min_num
+    while (min_num != difference) {
+        temp1 = min_num
+        min_num = min(temp1, difference)
+        difference = max(temp1, difference) - min_num
+    }
+    return (difference == 1)
+}
+
 
 /**
  * Простая
@@ -115,7 +172,12 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    var sqm: Int = sqrt(m.toDouble()).toInt()
+    var sqn: Int = sqrt(n.toDouble()).toInt()
+    return ((((m % sqm) == 0) || ((n % sqn) == 0)) || (sqm < sqn))
+}
+
 
 /**
  * Средняя
@@ -133,6 +195,8 @@ fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
+
+
 fun collatzSteps(x: Int): Int = TODO()
 
 /**
--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -136,7 +136,9 @@ class Tests {
         assertTrue(isCoPrime(25, 49))
         assertFalse(isCoPrime(6, 8))
         assertTrue(isCoPrime(17, 97))
+        assertTrue(isCoPrime(6, 13))
         assertFalse(isCoPrime(37, 111))
+        assertFalse(isCoPrime(33, 66))
         assertTrue(isCoPrime(1234567890, 908765431))
         assertTrue(isCoPrime(2109876543, 1234567891))
     }
